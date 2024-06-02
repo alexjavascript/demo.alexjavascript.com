e529e3e8656bc7b8e0b41eec6293ae3f9db16d07
Alex. <grigoriev.alex.97@gmail.com>
2024-06-02 14:40:39 +0500

    Update home page and add hounted house demo

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,8 @@ document.body.innerHTML = `
     body {
       margin: 0;
       display: grid;
-      grid-template-columns: repeat(1, 1fr);;
+      grid-template-columns: repeat(1, 1fr);
+      font-family: 'Arial';
     }
 
     @media (min-width: 480px) {
@@ -27,10 +28,12 @@ document.body.innerHTML = `
     }
 
     a {
+      position: relative;
       display: block;
       border-right: 1px solid rgba(255, 255, 255, 0.1);
       border-bottom: 1px solid rgba(255, 255, 255, 0.1);
       font: inherit;
+      aspect-ratio: 1;
     }
 
     img {
@@ -39,56 +42,104 @@ document.body.innerHTML = `
     }
 
     span {
-      display: none;
+      position: absolute;
+      left: 0;
+      top: 0;
+      color: white;
     }
   </style>
 
+  <a href="/work/5">
+    <img src="/public/images/work/5/preview.jpg">
+    <span>2023.11.03 R</span>
+  </a>
+
+  <a>
+    <span>2023.11.08 W</span>
+  </a>
+
+  <a>
+    <span>2023.11.07 T</span>
+  </a>
+
+  <a>
+    <span>2023.11.06 M</span>
+  </a>
+
+  <a>
+    <span>2023.11.05 Y</span>
+  </a>
+
+  <a>
+    <span>2023.11.04 S</span>
+  </a>
+
+  <a>
+    <span>2023.11.03 F</span>
+  </a>
+
+  <a>
+    <span>2023.11.02 R</span>
+  </a>
+  
+  <a>
+    <span>2023.11.01 W</span>
+  </a>
+  
+  <a>
+    <span>2023.10.31 T</span>
+  </a>
+  
+  <a>
+    <span>2023.10.30 M</span>
+  </a>
+  
+  <a>
+    <span>2023.10.29 Y</span>
+  </a>
+
+  <a>
+    <span>2023.10.28 S</span>
+  </a>
+  
+  <a>
+    <span>2023.10.27 F</span>
+  </a>
+
+  <a>
+    <span>2023.10.26 R</span>
+  </a>
+  
+  <a>
+    <span>2023.10.25 W</span>
+  </a>
+  
+  <a>
+    <span>2023.10.24 T</span>
+  </a>
+
   <a href="/work/4">
     <img src="/public/images/work/4/preview.png">
-    <span>2023.10.23</span>
+    <span>2023.10.23 M</span>
   </a>
 
   <a href="/work/3">
     <img src="/public/images/work/3/preview.png">
-    <span>2023.10.22</span>
+    <span>2023.10.22 Y</span>
   </a>
 
   <a href="/work/2">
     <img src="/public/images/work/2/preview.png">
-    <span>2023.10.22</span>
+    <span>2023.10.22 Y</span>
   </a>
   
   <a href="/work/1">
     <img src="/public/images/work/1/preview.png">
-    <span>2023.10.21</span>
+    <span>2023.10.21 S</span>
   </a>
 
   <a href="/work/0">
     <img src="/public/images/work/0/preview.jpg">
-    <span>2023.10.20</span>
-  </a>
-
-  <a href="/work/0">
-    <img src="/public/images/work/0/preview.jpg">
-  </a>
-
-  <a href="/work/0">
-    <img src="/public/images/work/0/preview.jpg">
-  </a>
-
-  <a href="/work/0">
-    <img src="/public/images/work/0/preview.jpg">
-  </a>
-
-  <a href="/work/0">
-    <img src="/public/images/work/0/preview.jpg">
-  </a>
-
-  <a href="/work/0">
-    <img src="/public/images/work/0/preview.jpg">
-  </a>
-
-  <a href="/work/0">
-    <img src="/public/images/work/0/preview.jpg">
+    <span>2023.10.20 F</span>
   </a>
 `
